08af478b869d5cc3a06ea270ecd79028af20c49b
Liivasoa <liva.ravelotahiana@gmail.com>
2024-11-05 14:20:03 +0100

    Feat-HomePage : Add other lunar phase on the claculator test

--- src/application/MoonPhaseCalculator.test.js
+++ src/application/MoonPhaseCalculator.test.js
@@ -1,32 +1,98 @@
 import {calculateMoonPhaseByDate} from './MoonPhaseCalculator.js';
 
 test('should return new-moon', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 6))).toBe('new-moon');
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 6))).toBe('new-moon');
 });
-test('should return new-moon', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 13))).toBe('new-moon');
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 7))).toBe('waxing-crescent');
 });
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 8))).toBe('waxing-crescent');
+});
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 9))).toBe('waxing-crescent');
+});
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 10))).toBe('waxing-crescent');
+});
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 11))).toBe('waxing-crescent');
+});
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 12))).toBe('waxing-crescent');
+});
+test('should return waxing-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 13))).toBe('waxing-crescent');
+});
+
 
 test('should return first-quarter', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 14))).toBe('first-quarter');
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 14))).toBe('first-quarter');
 });
-test('should return first-quarter', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 20))).toBe('first-quarter');
+test('should return waxing-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 15))).toBe('waxing-gibbous');
 });
+test('should return waxing-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 16))).toBe('waxing-gibbous');
+});
+test('should return waxing-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 17))).toBe('waxing-gibbous');
+});
+test('should return waxing-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 18))).toBe('waxing-gibbous');
+});
+test('should return waxing-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 19))).toBe('waxing-gibbous');
+});
+test('should return waxing-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 20))).toBe('waxing-gibbous');
+});
+
 
 test('should return full-moon', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 21))).toBe('full-moon');
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 21))).toBe('full-moon');
 });
-test('should return full-moon', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 27))).toBe('full-moon');
+test('should return waning-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 22))).toBe('waning-gibbous');
+});
+test('should return waning-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 23))).toBe('waning-gibbous');
+});
+test('should return waning-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 24))).toBe('waning-gibbous');
+});
+test('should return waning-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 25))).toBe('waning-gibbous');
+});
+test('should return waning-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 26))).toBe('waning-gibbous');
+});
+test('should return waning-gibbous', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 27))).toBe('waning-gibbous');
 });
 
 
-test('should return third-quarter', () => {
-    expect(calculateMoonPhaseByDate(Date(2000, 0, 28))).toBe('third-quarter');
+test('should return last-quarter', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 28))).toBe('last-quarter');
 });
-
-test('should throws exception', () => {
-    const date = 'not a date';
-    expect(calculateMoonPhaseByDate(date)).toThrow('Passed argument type must be a Date');
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 29))).toBe('waning-crescent');
+});
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 30))).toBe('waning-crescent');
+});
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 0, 31))).toBe('waning-crescent');
+});
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 1, 1))).toBe('waning-crescent');
+});
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 1, 2))).toBe('waning-crescent');
+});
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 1, 3))).toBe('waning-crescent');
+});
+test('should return waning-crescent', () => {
+    expect(calculateMoonPhaseByDate(new Date(2000, 1, 4))).toBe('waning-crescent');
 });
