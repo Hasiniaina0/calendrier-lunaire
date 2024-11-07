c60424f69e5856bd7c1b3e42ec05a6b40e6ab9ca
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-11-07 15:56:21 +0100

    Feat-HomePage : add calendar and change differents phases of the moon

--- src/component/DayView.jsx
+++ src/component/DayView.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-function DayView({ imageUrl, title }) {
+function DayView({ date, imageUrl, title }) {
   return (
     <div className="max-w-sm mx-auto mt-10 p-4 ">
+      <div className=" font-semibold text-4xl text-slate-100 ">
+        {date.toLocaleDateString()} <br />
+      </div>
       <img src={imageUrl} alt={title} className="w-full" />
       <h1 className="text-5xl font-semibold text-blue-300 text-center">
         {title}
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,14 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import fullMoon from "../asset/full-moon.png";
+import newMoon from "../asset/new-moon.png";
 import firstQuarter from "../asset/fisrt-quarter.png";
+import waningGibbous from "../asset/waning-gibbous.jpg";
+import waxingGibbous from "../asset/waxing-gibbous.png";
+import waxingCrescent from "../asset/fisrt-quarter.png";
+import waningCrescent from "../asset/waning-crescent.png";
+import lastQuarter from "../asset/last-quarter.jpg";
+import { calculateMoonPhaseByDate } from "../application/MoonPhaseCalculator";
 import DayView from "../component/DayView";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function HomePage() {
   const [phase, setPhase] = useState("Full moon");
   const [img, setImg] = useState(firstQuarter);
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const moonPhaseImages = {
+    "new-moon": newMoon,
+    "waxing-crescent": waxingCrescent,
+    "first-quarter": firstQuarter,
+    "waxing-gibbous": waxingGibbous,
+    "full-moon": fullMoon,
+    "waning-gibbous": waningGibbous,
+    "last-quarter": lastQuarter,
+    "waning-crescent": waningCrescent,
+  };
+
+  // Effectuer un calcul de la phase à chaque fois que la date change
+  useEffect(() => {
+    const currentPhase = calculateMoonPhaseByDate(currentDate); // Calculer la phase de la lune actuelle
+    setPhase(currentPhase); // Mettre à jour l'état avec la phase de la lune
+    setImg(moonPhaseImages[currentPhase]); // Mettre à jour l'état avec l'image de la lune correspondant à la phase
+  }, [currentDate]);
+
   return (
     <div>
-      <DayView imageUrl={img} title={phase} />
+      <p className="text-red-50">
+        Séléctionnez une date :
+        <DatePicker
+          className="border-4 border-solid bg-slate-400"
+          selected={currentDate}
+          onChange={(date) => setCurrentDate(date)}
+          showIcon
+          dateFormat="dd/MM/yyyy"
+        />
+      </p>
+      <DayView date={currentDate} imageUrl={img} title={phase} />
     </div>
   );
 }
