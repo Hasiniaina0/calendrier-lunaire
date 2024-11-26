684c696c34ffefe7dfbaec75a247ed152b3a8b4e
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-11-26 11:14:57 +0100

    Feat-HomePage : CALLU-13 calendar displaying when clicking on the icon and CALLU-20 create component IconeView

--- src/component/DateSelectorView.jsx
+++ src/component/DateSelectorView.jsx
@@ -1,11 +1,24 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import PropTypes from "prop-types";
+import IconeView from "./IconeView";
+import { useRef } from "react";
 
 const DateSelectorView = ({ selectedDate, handleDateChange }) => {
+  const datePickerRef = useRef(null); // permet de référencer une valeur qui n'est pas nécessaire pour le rendu
+
+  const handleIconClick = () => {
+    if (datePickerRef.current) {
+      //clé qui vous donne accès à ce que la référence pointe
+      datePickerRef.current.setFocus(); // Active le focus pour afficher le calendrier
+    }
+  };
+
   return (
-    <div className=" text-white">
+    <div className="flex items-center space-x-2 text-white ">
+      <IconeView onIconClick={handleIconClick} />
       <DatePicker
+        ref={datePickerRef} // stocke la date choisie
         selected={selectedDate}
         onChange={handleDateChange}
         className="bg-transparent  text-stone-400 focus:outline-none "
