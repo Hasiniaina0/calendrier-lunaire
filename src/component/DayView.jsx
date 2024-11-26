7b496564b5cc80a4708cfb420e78d4581115420f
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-11-26 12:05:09 +0100

    Feat-HomePage : CALLU-19 change the DayView component to put the moon phase in a single line

--- src/component/DayView.jsx
+++ src/component/DayView.jsx
@@ -3,12 +3,12 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function DayView({ imageUrl, title }) {
   return (
-    <div className="max-w-sm mx-auto mt-10 p-4 ">
+    <div className="max-w-md mx-auto mt-10 p-4 ">
       <LazyLoadImage
         src={imageUrl}
         alt={title}
         placeholderSrc="Phase de la lune"
-        className="rounded-full border-spacing-52 border-8 border-emerald-950"
+        className="rounded-full border-spacing-52 border-8 border-emerald-950 w-96 h-96   "
       />
 
       <h1 className=" mt-10 text-4xl font-semibold text-yellow-200 text-center opacity-50">
