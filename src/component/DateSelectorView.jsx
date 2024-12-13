2bb8c0dbbebc23bc7f0842c54178f7934bf9e61c
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-12-13 15:57:37 +0100

    CALLU-1: Make date selector center on page

--- src/component/DateSelectorView.jsx
+++ src/component/DateSelectorView.jsx
@@ -13,25 +13,23 @@ const DateSelectorView = ({ selectedDate, handleDateChange }) => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full max-w-xs mx-auto space-x-2">
-      <div onClick={handleIconClick} className="flex-shrink-0 cursor-pointer">
-        <svg
-          aria-label="Calendar icon"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          className="w-6 h-6 text-zinc-500"
-        >
-          <path d="M19 3h-1V2a1 1 0 10-2 0v1H8V2a1 1 0 10-2 0v1H5a2 2 0 00-2 2v15a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2zm0 17H5V9h14v11zM7 11h5v5H7v-5z"></path>
-        </svg>
-      </div>
+    <div className="flex gap-2 ml-20">
+      <svg
+        onClick={handleIconClick}
+        aria-label="Calendar icon"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        className="w-6 h-6 text-zinc-500 flex-shrink-0 cursor-pointer "
+      >
+        <path d="M19 3h-1V2a1 1 0 10-2 0v1H8V2a1 1 0 10-2 0v1H5a2 2 0 00-2 2v15a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2zm0 17H5V9h14v11zM7 11h5v5H7v-5z"></path>
+      </svg>
       <DatePicker
         ref={datePickerRef}
         selected={selectedDate}
         onChange={handleDateChange}
-        className="w-full text-center bg-transparent"
-        containerClassName="w-full"
+        className="bg-transparent"
         dateFormat="dd/MM/yyyy"
         calendarClassName="bg-gray-800 rounded-lg shadow-xl"
         monthClassName="text-center text-lg font-semibold text-indigo-400"
