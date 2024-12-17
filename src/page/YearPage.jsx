e9bc32717611fba138c1d9b3255620e1b2d2d536
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-12-17 18:21:45 +0100

    Add information to YearPage

--- src/page/YearPage.jsx
+++ src/page/YearPage.jsx
@@ -1,10 +1,15 @@
 import Menu from "../component/Menu";
+import { MdOutlineConstruction } from "react-icons/md";
 function YearPage() {
   return (
     <div>
       <Menu />
-
-      <div className=""></div>
+      <div className="flex flex-col items-center justitfy-center text-white mt-2">
+        <MdOutlineConstruction className="text-9xl text-white" />
+        <div className="text-3xl text-white">
+          <h1>Chantier en cours ...</h1>
+        </div>
+      </div>
     </div>
   );
 }
